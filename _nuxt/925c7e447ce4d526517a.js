15ccdafa91055a58342cffd061dcb3b0a3ba2eb7
Icey Isaak <me@iceyisaak.com>
2020-05-23 11:24:53 +0200

    9a22eb7

---
+++ _nuxt/925c7e447ce4d526517a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{147:function(t,n,e){"use strict";e.r(n);var l=e(24),component=Object(l.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("h1",[this._v("Sandbox_GithubPage-Nuxt")])}),[],!1,null,null,null);n.default=component.exports}}]);
